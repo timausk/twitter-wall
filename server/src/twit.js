f924f6b7fa5d26e9da237e71a82180d84b5f44ee
timausk <timausk@googlemail.com>
2020-06-01 08:16:29 +0200

    [server] get alternative avatar size

--- server/src/twit.js
+++ server/src/twit.js
@@ -1,4 +1,7 @@
 const twit = require('twit');
+const defaultAvatarSizes = 'normal';
+const availableAvatarSizes = ['mini', 'bigger', 'original'];
+
 require('dotenv').config();
 
 let Twitter = new twit({
@@ -10,32 +13,39 @@ let Twitter = new twit({
   strictSSL: true
 });
 
-function filterTweets (tweets) {
+const filterTweets = (tweets) => {
   return tweets.map(tweet => ({
     created_at: formatDate(tweet.created_at),
     user_name: tweet.user.name,
     user_screen_name: tweet.user.screen_name,
-    user_avatar: tweet.user.profile_image_url_https,
+    user_avatar: process.env.avatar_size
+      ? getAlternativeAvatarSize(tweet.user.profile_image_url_https, process.env.avatar_size)
+      : tweet.user.profile_image_url_https,
     tweet_text: tweet.text,
     tweet_retweeted_status_text: tweet.retweeted_status
       ? tweet.retweeted_status.text
       : null,
   }));
-}
+};
 
-function formatDate (dateStr) {
+const formatDate = (dateStr) => {
   const date = new Date(dateStr);
   const dateTimeFormat = new Intl.DateTimeFormat('en',{year:'numeric',month:'short',day:'2-digit'});
   const [{ value: month },,{ value: day },,{ value: year }] = dateTimeFormat .formatToParts(date );
   return `${day} ${month} ${year}`;
-}
+};
+
+const getAlternativeAvatarSize = (imageUrl, size) => {
+  return (size === defaultAvatarSizes || ! availableAvatarSizes.includes(size))
+    ? imageUrl
+    : imageUrl.replace(defaultAvatarSizes, size);
+};
 
 export default Object.assign({
   async searchTweets(query, maxResults = 100) {
     const url = 'search/tweets';
     const params = {q: `${query}`, count: maxResults};
     const result = await Twitter.get(url, params);
-    //console.log(result.data.statuses);
     return filterTweets(result.data.statuses);
   }
 });
